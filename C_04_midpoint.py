c1bc3348f9ddcb8d28888ae010c480272d69d2f8
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-05-01 12:41:33 +1200

    Renamed a few variables added some comments

--- C_04_midpoint.py
+++ C_04_midpoint.py
@@ -18,13 +18,15 @@ def num_check(question):
             print(error)
 
 
-# calculates the gradient of the 2 points
+# calculates the midpoint of the 2 points
 def calc_midpoint(x1, y1, x2, y2):
 
     # finds the x and y coordinates for the midpoints
     x_mid = (x2 + x1) / 2
     y_mid = (y2 + y1) / 2
 
+    # puts the x and y middle points into one message
+    # so that it displays as a coordinate
     midpoint = f"{x_mid}, {y_mid}"
 
     return midpoint
@@ -36,6 +38,6 @@ first_y = num_check("What is your first y point?")
 second_x = num_check("What is your second x point?")
 second_y = num_check("What is your second y point?")
 
-gradient = calc_midpoint(first_x, first_y, second_x, second_y)
+midpoint = calc_midpoint(first_x, first_y, second_x, second_y)
 
-print(gradient)
+print(midpoint)
