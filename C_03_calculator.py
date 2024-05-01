19600a14572c3ad44d073de1d98e8fa297b92d37
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-05-01 13:55:08 +1200

    Integrated all the calculator functions into one component

--- C_03_calculator.py
+++ C_03_calculator.py
@@ -1,3 +1,6 @@
+import math
+
+
 # checks that the input is a number
 
 def num_check(question):
@@ -19,16 +22,36 @@ def num_check(question):
 
 
 # calculates the gradient of the 2 points
-def gradient(x1, y1, x2, y2):
-
+def calc_gradient(x1, y1, x2, y2):
     # finds the difference in the 2 x and y points
-    diff_in_y = y1 - y2
-    diff_in_x = x1 - x2
+    diff_in_y = y2 - y1
+    diff_in_x = x2 - x1
 
     # finds the actual gradient
     gradient = diff_in_y / diff_in_x
 
     return gradient
+
+
+# calculates the midpoint of the 2 points
+def calc_midpoint(x1, y1, x2, y2):
+    # finds the x and y coordinates for the midpoints
+    x_mid = (x2 + x1) / 2
+    y_mid = (y2 + y1) / 2
+
+    # puts the x and y middle points into one message
+    # so that it displays as a coordinate
+    midpoint = f"{x_mid}, {y_mid}"
+
+    return midpoint
+
+
+# calculates the distance of the 2 points
+def calc_distance(x1, y1, x2, y2):
+    # calculates the distance between the two points
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    return distance
 
 
 # gets the 2 x and y points
@@ -37,6 +60,13 @@ first_y = num_check("What is your first y point?")
 second_x = num_check("What is your second x point?")
 second_y = num_check("What is your second y point?")
 
-gradient = gradient(first_x, first_y, second_x, second_y)
+# gets all the calculations
+gradient = calc_gradient(first_x, first_y, second_x, second_y)
+midpoint = calc_midpoint(first_x, first_y, second_x, second_y)
+distance = calc_distance(first_x, first_y, second_x, second_y)
+equation = f"y = {gradient}x + {((gradient * first_x) * -1) + first_y}"
 
-print(gradient)
+print(f"Equation: {equation}")
+print(f"Gradient: {gradient}")
+print(f"Midpoint: {midpoint}")
+print(f"Distance: {distance}")
