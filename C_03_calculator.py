a6e9c34b42af2e8f03089b5eaa4945cd2f162130
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-05-02 14:59:11 +1200

    Added rounding to 2dp for answers

--- C_03_calculator.py
+++ C_03_calculator.py
@@ -41,7 +41,7 @@ def calc_midpoint(x1, y1, x2, y2):
 
     # puts the x and y middle points into one message
     # so that it displays as a coordinate
-    midpoint = f"{x_mid}, {y_mid}"
+    midpoint = f"{x_mid:.2f}, {y_mid:.2f}"
 
     return midpoint
 
@@ -64,10 +64,12 @@ second_y = num_check("What is your second y point?")
 gradient = calc_gradient(first_x, first_y, second_x, second_y)
 midpoint = calc_midpoint(first_x, first_y, second_x, second_y)
 distance = calc_distance(first_x, first_y, second_x, second_y)
+
+# finds the equation for between the two points
 y_intercept = first_y - gradient * first_x
-equation = f"y = {gradient}x + {y_intercept}"
+equation = f"y = {gradient:.2f}x + {y_intercept:.2f}"
 
 print(f"Equation: {equation}")
-print(f"Gradient: {gradient}")
+print(f"Gradient: {gradient:.2f}")
 print(f"Midpoint: {midpoint}")
-print(f"Distance: {distance}")
+print(f"Distance: {distance:.2f}")
