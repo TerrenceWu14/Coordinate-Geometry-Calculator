5dd4cb1a0d670e2000c39a4a7e1c669402bec3dc
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-05-08 12:29:47 +1200

    Almost finished third iteration in expirementation

---
+++ expirementing.py
@@ -0,0 +1,102 @@
+import math
+import re
+
+
+# checks that the input is a number
+
+def num_check(question):
+    while True:
+
+        # sets up error message
+        error = "Please only enter numbers"
+
+        # tries to convert number to a float
+
+        try:
+            number = float(input(question))
+            return number
+
+        # if it can't convert the input into a float it then prints the error message and sends the user back to the
+        # start of the loop
+        except ValueError:
+            print(error)
+
+
+# calculates the gradient of the 2 points
+def calc_gradient(x1, y1, x2, y2):
+    # finds the difference in the 2 x and y points
+    diff_in_y = y2 - y1
+    diff_in_x = x2 - x1
+
+    # finds the actual gradient
+    gradient = diff_in_y / diff_in_x
+
+    return gradient
+
+
+# calculates the midpoint of the 2 points
+def calc_midpoint(x1, y1, x2, y2):
+    # finds the x and y coordinates for the midpoints
+    x_mid = (x2 + x1) / 2
+    y_mid = (y2 + y1) / 2
+
+    # puts the x and y middle points into one message
+    # so that it displays as a coordinate
+    midpoint = f"{x_mid:.2f}, {y_mid:.2f}"
+
+    return midpoint
+
+
+# calculates the distance of the 2 points
+def calc_distance(x1, y1, x2, y2):
+    # calculates the distance between the two points
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    return distance
+
+
+# gets the 2 x and y points
+first_x = 19
+first_y = 5
+second_x = 25
+second_y = 21
+
+# does all the calculations using the functions
+gradient = calc_gradient(first_x, first_y, second_x, second_y)
+midpoint = calc_midpoint(first_x, first_y, second_x, second_y)
+distance = calc_distance(first_x, first_y, second_x, second_y)
+
+# finds the equation for between the two points
+y_intercept = first_y - gradient * first_x
+equation = f"y = {gradient:.2f}x + {y_intercept:.2f}"
+
+# sets up dict
+possible_answers = {
+    "equation": equation,
+    "midpoint": midpoint,
+    "distance": distance,
+    "gradient": gradient,
+}
+
+keep_going = ""
+
+while keep_going == "":
+    # asks the user what answers they'd like
+    wanted_answers = input("What answers would you like? ").lower()
+
+    # converts the user input into a list of items they want
+    wanted_answers = re.split(r',|\s+|\band*?\b', wanted_answers.strip())
+    print(wanted_answers)
+
+    # prints the answers the user wants
+    for item in wanted_answers:
+
+        # Check if the item is in the possible_answers dictionary
+        if item in possible_answers:
+            print(f"{item}: {possible_answers[item]}")
+
+            keep_going = input("Do you need any other answers (<enter> for yes)? ")
+
+        else:
+            print(f"Please enter in equation, midpoint, distance or gradient only "
+                  f"(or check your spelling).")
