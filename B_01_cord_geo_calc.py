9ace6f16d9c6d33b28899fc444d71658bc88df67
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-05-24 11:51:21 +1200

    Integrated the yes/no function and instructions

--- B_01_cord_geo_calc.py
+++ B_01_cord_geo_calc.py
@@ -1,6 +1,52 @@
 import math
 
 
+# checks that the user typed yes or no
+def yes_no(question):
+    while True:
+
+        # asks the question
+        response = input(question).lower()
+
+        # sets up yes or no dict
+        yes_or_no = {
+            "y": "yes",
+            "n": "no",
+        }
+
+        # checks if the first letter of the response
+        # fits the keys in the dict
+        if response[:1] in yes_or_no:
+            return yes_or_no[response]
+
+        # prints the error message
+        else:
+            print("Please enter yes or no\n")
+
+
+# Displays instructions
+def instructions():
+    print('''
+
+***** Instructions *****
+
+To begin, you enter your first x and y coordinates along with
+the second one. Then Enter what answer's you'd like to see, for
+example: "equation" and it will give you the equation according 
+to the two x and y points you entered. 
+
+The list of answers that we can calculate for you are: 
+equation, gradient, midpoint and distance.
+
+After we give you your answer we also put it all onto a txt file 
+for you to view or share with others if you'd like to do so.
+
+That's about it, thanks for using my coordinate geometry calculator! 
+
+*****************************************************************
+    ''')
+
+
 # checks that the input is a number
 
 def num_check(question):
@@ -94,6 +140,11 @@ def print_answer(question, allowed_responses, answers):
 # sets up to_write list
 to_write = []
 
+want_instructions = yes_no("Do you want to see the instructions?")
+
+if want_instructions == "yes":
+    instructions()
+
 while True:
 
     # gets the 2 x and y points
@@ -114,7 +165,6 @@ while True:
 
     else:
         break
-
 
 # finds the equation for between the two points
 y_intercept = first_y - gradient * first_x
