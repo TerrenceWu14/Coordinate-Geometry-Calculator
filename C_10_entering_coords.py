fddc9300d04bf86332d0d81003c1af92a71989ce
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-06-13 12:28:12 +1200

    End of period
    Tried experimenting with how to extract a value from a list but nothing worked so I just reverted it back to the first thing I did after the previous commit

--- C_10_entering_coords.py
+++ C_10_entering_coords.py
@@ -5,10 +5,18 @@ pattern = r'[+-]?([0-9]*[.])?[0-9]+'
 while True:
     response = input()
 
-    if re.match(pattern, response):
-        float(response)
-        print(response)
+    response = re.sub(r'[(),]', r'', response)
+    print(response)
 
-    else:
-        print("Only enter floats")
-        continue
+    response = re.split(r'', response)
+
+    print(response)
+
+    #
+    # if re.match(pattern, response):
+    #     float(response)
+    #     print(response)
+    #
+    # else:
+    #     print("Only enter floats")
+    #     continue
