cbcf944b070ed4d1095ae774ac1a5133216355e5
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-06-14 13:29:33 +1200

    Trialing solutions
    I forgot to commit a previous trial which was using re.search

--- C_10_entering_coords.py
+++ C_10_entering_coords.py
@@ -5,7 +5,10 @@ pattern = r'[(]?[+-]?([0-9]*[.])?[0-9]+[)]?'
 
 temp = 0
 
-while temp < 2:
+first_x = None
+first_y = None
+
+while True:
 
     # gets the user's coordinates
     response = input()
@@ -14,25 +17,26 @@ while temp < 2:
     if re.match(pattern, response):
 
         # subs the brackets and commas with a whitespace
-        response = re.sub(r'[(),]', r'', response)
+        response = re.sub(r'[(),]', r' ', response)
 
-        # turns the response into a list
-        response = list(response)
+        response = re.findall(r'-?([0-9]*[.])?[0-9]', response)
 
-        # sets the second coordinates
-        if temp == 1:
-            second_x = response[0]
-            second_y = response[1]
-            print(f"Second x: {second_x} and Second y: {second_y}")
-            temp += 1
+        print(response)
 
-        # sets the coordinate to their respective variables
-        first_x = response[0]
-        first_y = response[1]
-
-        print(f"First x: {first_x} and First y: {first_y}")
-
-        temp += 1
-
-    else:
-        print("Only enter floats")
+    #     # sets the second coordinates
+    #     if temp == 1:
+    #         second_x = response[0]
+    #         second_y = response[1]
+    #
+    #         print(f"First x: {first_x} and First y: {first_y}")
+    #         print(f"Second x: {second_x} and Second y: {second_y}")
+    #         temp += 1
+    #
+    #     # sets the coordinate to their respective variables
+    #     first_x = response[0]
+    #     first_y = response[1]
+    #
+    #     temp += 1
+    #
+    # else:
+    #     print("Only enter floats")
