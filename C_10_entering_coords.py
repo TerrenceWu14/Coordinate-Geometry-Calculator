3b87f8364c6475973385a536cae04741f1bbe796
Terrence <135181592+TerrenceWu14@users.noreply.github.com>
2024-06-20 10:50:53 +1200

    Solved the problem for floats

--- C_10_entering_coords.py
+++ C_10_entering_coords.py
@@ -3,40 +3,25 @@ import re
 # sets the pattern allowed
 pattern = r'[(]?[+-]?([0-9]*[.])?[0-9]+[)]?'
 
-temp = 0
-
-first_x = None
-first_y = None
-
 while True:
 
     # gets the user's coordinates
-    response = input()
+    response = input("Enter a coordinate (e.g. 3,4 or (5,2)) ")
 
     # if the response matches the pattern it plays the code
     if re.match(pattern, response):
 
-        # subs the brackets and commas with a whitespace
-        response = re.sub(r'[(),]', r' ', response)
+        # removes the brackets and white space
+        response = re.sub(r'[()\s]', r'', response)
 
-        response = re.findall(r'-?([0-9]*[.])?[0-9]', response)
+        # sets the x to the first num and y to second num after split
+        x_str, y_str = response.split(',')
 
-        print(response)
+        # turns the response into a list
+        response = list(response)
 
-    #     # sets the second coordinates
-    #     if temp == 1:
-    #         second_x = response[0]
-    #         second_y = response[1]
-    #
-    #         print(f"First x: {first_x} and First y: {first_y}")
-    #         print(f"Second x: {second_x} and Second y: {second_y}")
-    #         temp += 1
-    #
-    #     # sets the coordinate to their respective variables
-    #     first_x = response[0]
-    #     first_y = response[1]
-    #
-    #     temp += 1
-    #
-    # else:
-    #     print("Only enter floats")
+        first_x = float(x_str)
+        first_y = float(y_str)
+        print(f"First x: {first_x} and First y: {first_y}")
+    else:
+        print("Only enter floats")
